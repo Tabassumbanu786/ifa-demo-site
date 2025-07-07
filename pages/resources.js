7ee758e51fdaf44085ea50cad89d38e8573e9727
User <user@Users-MacBook-Pro.local>
2025-07-07 15:52:28 +0530

    fix: cleaned up import path for SipCalculator

--- pages/resources.js
+++ pages/resources.js
@@ -3,8 +3,7 @@ import React from "react";
 import dynamic from "next/dynamic";
 import Layout from "@/components/Layout";
 import ResourcesSection from "@/components/ResourcesSection";
-import SipCalculator from "@/components/SipCalculator";
-import EmiCalculator from "@/components/EmiCalculator";
+
 
 
 
@@ -12,6 +11,7 @@ export default function ResourcesPage() {
   return (
     <Layout>
       <ResourcesSection />
+
     </Layout>
   );
 }
